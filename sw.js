241034a6f73be51ed5d825c4e073f079aafdce63
AxiomDeveloper <sqtsqqhtkx@privaterelay.appleid.com>
2026-02-07 11:56:29 +0000

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE = "repo-dumper-v1";
+const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.json"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE ? caches.delete(k) : null)))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  const url = new URL(e.request.url);
+  if(url.origin === location.origin){
+    e.respondWith(
+      caches.match(e.request).then((cached) => cached || fetch(e.request))
+    );
+  }
+});
